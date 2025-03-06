ec4955efdb804668134121b9149d24d5441aa3b0
Hijazi Mohamad Ali <mohamad.ali.hijazi@student.uni-miskolc.hu>
2025-03-06 10:15:40 +0100

    added funciton and calculation of tangent

--- src/spline_panel.js
+++ src/spline_panel.js
@@ -1,85 +1,39 @@
 const POINT_RADIUS = 10;
 
 /**
- * A panel for drawing splines.
+ * A panel for drawing splines and calculating Hermite interpolations.
  */
-class SplinePanel extends Panel
-{
-  /**
-   * Construct a new spline panel.
-   */
-  constructor(mock)
-  {
+class SplinePanel extends Panel {
+  constructor(mock) {
     super();
     this._points = [
-      {x: 200, y: 200},
-      {x: 400, y: 200},
-      {x: 200, y: 400},
-      {x: 400, y: 400},
+      { u: 0, p: [200, 200] },
+      { u: 1, p: [400, 200] },
+      { u: 2, p: [200, 400] },
+      { u: 3, p: [400, 400] },
     ];
     this._selectedPoint = null;
   }
 
-  /**
-   * Handle the mouse down event.
-   */
-  onMouseDown(mouse)
-  {
-    console.log("Mouse down");
-    console.log(mouse);
+  onMouseDown(mouse) {
     this._selectedPoint = this.findPoint(mouse.x, mouse.y);
   }
 
-  /**
-  * Handle the mouse move event.
-  */
-  onMouseMove(mouse)
-  {
-    console.log("Mouse move");
-    console.log(mouse);
+  onMouseMove(mouse) {
     if (this._selectedPoint != null) {
-      this._selectedPoint.x = mouse.x;
-      this._selectedPoint.y = mouse.y;
+      this._selectedPoint.p = [mouse.x, mouse.y];
+      this.requireRedraw();
     }
-    this.requireRedraw();
   }
 
-  /**
-   * Handle the mouse up event.
-   */
-  onMouseUp(mouse)
-  {
-    console.log("Mouse up");
-    console.log(mouse);
+  onMouseUp(mouse) {
     this._selectedPoint = null;
   }
 
-  /**
-   * Handle the key down event.
-   */
-  onKeyDown(key)
-  {
-    console.log("Key down");
-    console.log(key);
-  }
-
-  /**
-   * Handle the key up event.
-   */
-  onKeyUp(key)
-  {
-    console.log("Key up");
-    console.log(key);
-  }
-
-  /**
-   * Find a control point at the given coordinates.
-   */
-  findPoint(x, y)
-  {
-    for (var point of this._points) {
-      const dx = point.x - x;
-      const dy = point.y - y;
+  findPoint(x, y) {
+    for (let point of this._points) {
+      const dx = point.p[0] - x;
+      const dy = point.p[1] - y;
       let distance = Math.sqrt(dx * dx + dy * dy);
       if (distance < POINT_RADIUS) {
         return point;
@@ -88,47 +42,82 @@ class SplinePanel extends Panel
     return null;
   }
 
-  /**
-   * Draw the content of the panel.
-   */
-  draw(context)
-  {
-    context.fillStyle = "#FFF";
-    context.fillRect(0, 0, this.width, this.height);
-    this.drawPoints(context);
-    context.strokeStyle = "#AAA";
-    context.beginPath();
-    context.moveTo(this._points[0].x, this._points[0].y);
-    for (let i = 1; i < this._points.length; i++) {
-      context.lineTo(this._points[i].x, this._points[i].y);
+  calculateTangent(i) {
+    if (i === 0) {
+      return [
+        (this._points[i + 1].p[0] - this._points[i].p[0]) * 0.5,
+        (this._points[i + 1].p[1] - this._points[i].p[1]) * 0.5,
+      ];
+    } else if (i === this._points.length - 1) {
+      return [
+        (this._points[i].p[0] - this._points[i - 1].p[0]) * 0.5,
+        (this._points[i].p[1] - this._points[i - 1].p[1]) * 0.5,
+      ];
+    } else {
+      return [
+        (this._points[i + 1].p[0] - this._points[i - 1].p[0]) * 0.5,
+        (this._points[i + 1].p[1] - this._points[i - 1].p[1]) * 0.5,
+      ];
     }
-    context.stroke();
   }
 
-  /**
-   * Draw the control points.
-   */
-  drawPoints(context)
-  {
+  hermiteInterpolation(p0, p1, t0, t1, u) {
+    let h00 = (2 * u ** 3) - (3 * u ** 2) + 1;
+    let h10 = (u ** 3) - (2 * u ** 2) + u;
+    let h01 = (-2 * u ** 3) + (3 * u ** 2);
+    let h11 = (u ** 3) - (u ** 2);
+
+    let x = h00 * p0[0] + h10 * t0[0] + h01 * p1[0] + h11 * t1[0];
+    let y = h00 * p0[1] + h10 * t0[1] + h01 * p1[1] + h11 * t1[1];
+    return [x, y];
+  }
+
+  draw(context) {
+    context.fillStyle = "#FFF";
+    context.fillRect(0, 0, this.width, this.height);
+
+    context.strokeStyle = "#F00";
+    context.beginPath();
+    for (let i = 0; i < this._points.length - 1; i++) {
+      let p0 = this._points[i].p;
+      let p1 = this._points[i + 1].p;
+      let t0 = this.calculateTangent(i);
+      let t1 = this.calculateTangent(i + 1);
+
+      let firstHermite = this.hermiteInterpolation(p0, p1, t0, t1, 0);
+      context.moveTo(firstHermite[0], firstHermite[1]);
+
+      for (let u = 0; u <= 1; u += 0.02) {
+        let [x, y] = this.hermiteInterpolation(p0, p1, t0, t1, u);
+        context.lineTo(x, y);
+      }
+    }
+    context.stroke();
+
+    this.drawPoints(context);
+    this.drawLines(context);
+  }
+
+  drawPoints(context) {
     for (const point of this._points) {
       this.drawPoint(context, point);
     }
   }
 
-  /**
-   * Draw a control point.
-   */
-  drawPoint(context, point)
-  {
+  drawPoint(context, point) {
     context.strokeStyle = "#00F";
     context.beginPath();
-    context.moveTo(point.x - POINT_RADIUS, point.y);
-    context.lineTo(point.x + POINT_RADIUS, point.y);
-    context.stroke()
+    context.arc(point.p[0], point.p[1], POINT_RADIUS, 0, Math.PI * 2);
+    context.stroke();
+  }
+
+  drawLines(context) {
+    context.strokeStyle = "#AAA";
     context.beginPath();
-    context.moveTo(point.x, point.y - POINT_RADIUS);
-    context.lineTo(point.x, point.y + POINT_RADIUS);
-    context.stroke()
+    context.moveTo(this._points[0].p[0], this._points[0].p[1]);
+    for (let i = 1; i < this._points.length; i++) {
+      context.lineTo(this._points[i].p[0], this._points[i].p[1]);
+    }
+    context.stroke();
   }
 }
-
