68bf9009607bde337566b34dbd5e0c75a8c78527
Hijazi Mohamad Ali <mohamad.ali.hijazi@student.uni-miskolc.hu>
2025-03-06 09:26:22 +0100

    update code

--- src/spline_panel.js
+++ src/spline_panel.js
@@ -1,22 +1,21 @@
 const POINT_RADIUS = 10;
 
 /**
- * A panel for drawing splines and calculating the Lagrange interpolation.
+ * A panel for drawing splines.
  */
-class SplinePanel extends Panel {
+class SplinePanel extends Panel
+{
   /**
    * Construct a new spline panel.
    */
-  constructor(mock) {
+  constructor(mock)
+  {
     super();
-    // Initial points (can be moved)
     this._points = [
-      { u: 0, p: [300, 300] },
-      { u: 1, p: [600, 300] },
-      { u: 2, p: [300, 600] },
-      { u: 3, p: [600, 600] },
-      { u: 4, p: [900, 600] },
-      { u: 5, p: [600, 900] },
+      {x: 200, y: 200},
+      {x: 400, y: 200},
+      {x: 200, y: 400},
+      {x: 400, y: 400},
     ];
     this._selectedPoint = null;
   }
@@ -24,34 +23,63 @@ class SplinePanel extends Panel {
   /**
    * Handle the mouse down event.
    */
-  onMouseDown(mouse) {
+  onMouseDown(mouse)
+  {
+    console.log("Mouse down");
+    console.log(mouse);
     this._selectedPoint = this.findPoint(mouse.x, mouse.y);
   }
 
   /**
-   * Handle the mouse move event.
-   */
-  onMouseMove(mouse) {
+  * Handle the mouse move event.
+  */
+  onMouseMove(mouse)
+  {
+    console.log("Mouse move");
+    console.log(mouse);
     if (this._selectedPoint != null) {
-      this._selectedPoint.p = [mouse.x, mouse.y];
-      this.requireRedraw();
+      this._selectedPoint.x = mouse.x;
+      this._selectedPoint.y = mouse.y;
     }
+    this.requireRedraw();
   }
 
   /**
    * Handle the mouse up event.
    */
-  onMouseUp(mouse) {
+  onMouseUp(mouse)
+  {
+    console.log("Mouse up");
+    console.log(mouse);
     this._selectedPoint = null;
+  }
+
+  /**
+   * Handle the key down event.
+   */
+  onKeyDown(key)
+  {
+    console.log("Key down");
+    console.log(key);
+  }
+
+  /**
+   * Handle the key up event.
+   */
+  onKeyUp(key)
+  {
+    console.log("Key up");
+    console.log(key);
   }
 
   /**
    * Find a control point at the given coordinates.
    */
-  findPoint(x, y) {
-    for (let point of this._points) {
-      const dx = point.p[0] - x;
-      const dy = point.p[1] - y;
+  findPoint(x, y)
+  {
+    for (var point of this._points) {
+      const dx = point.x - x;
+      const dy = point.y - y;
       let distance = Math.sqrt(dx * dx + dy * dy);
       if (distance < POINT_RADIUS) {
         return point;
@@ -61,56 +89,27 @@ class SplinePanel extends Panel {
   }
 
   /**
-   * Compute the Lagrange interpolation for a given parameter u.
-   */
-  lagrangeInterpolation(points, u) {
-    let n = points.length;
-    let dimension = points[0].p.length;
-    let result = new Array(dimension).fill(0);
-
-    for (let i = 0; i < n; i++) {
-      let L_i = 1;
-      for (let j = 0; j < n; j++) {
-        if (i !== j) {
-          L_i *= (u - points[j].u) / (points[i].u - points[j].u);
-        }
-      }
-      for (let d = 0; d < dimension; d++) {
-        result[d] += L_i * points[i].p[d];
-      }
-    }
-    return result;
-  }
-
-  /**
    * Draw the content of the panel.
    */
-  draw(context) {
+  draw(context)
+  {
     context.fillStyle = "#FFF";
     context.fillRect(0, 0, this.width, this.height);
-
-    // Draw Lagrange interpolation curve
-    context.strokeStyle = "#F00";
+    this.drawPoints(context);
+    context.strokeStyle = "#AAA";
     context.beginPath();
-    let firstPoint = this.lagrangeInterpolation(this._points, 0);
-    context.moveTo(firstPoint[0], firstPoint[1]);
-
-    for (let u = 0; u <= this._points.length - 1; u += 0.01) {
-      let [x, y] = this.lagrangeInterpolation(this._points, u);
-      if (x >= 0 && x <= this.width && y >= 0 && y <= this.height) {
-        context.lineTo(x, y);
-      }
+    context.moveTo(this._points[0].x, this._points[0].y);
+    for (let i = 1; i < this._points.length; i++) {
+      context.lineTo(this._points[i].x, this._points[i].y);
     }
     context.stroke();
-
-    this.drawPoints(context);
-    this.drawLines(context);
   }
 
   /**
    * Draw the control points.
    */
-  drawPoints(context) {
+  drawPoints(context)
+  {
     for (const point of this._points) {
       this.drawPoint(context, point);
     }
@@ -119,23 +118,17 @@ class SplinePanel extends Panel {
   /**
    * Draw a control point.
    */
-  drawPoint(context, point) {
+  drawPoint(context, point)
+  {
     context.strokeStyle = "#00F";
     context.beginPath();
-    context.arc(point.p[0], point.p[1], POINT_RADIUS, 0, Math.PI * 2);
-    context.stroke();
-  }
-
-  /**
-   * Draw the lines connecting control points.
-   */
-  drawLines(context) {
-    context.strokeStyle = "#AAA";
+    context.moveTo(point.x - POINT_RADIUS, point.y);
+    context.lineTo(point.x + POINT_RADIUS, point.y);
+    context.stroke()
     context.beginPath();
-    context.moveTo(this._points[0].p[0], this._points[0].p[1]);
-    for (let i = 1; i < this._points.length; i++) {
-      context.lineTo(this._points[i].p[0], this._points[i].p[1]);
-    }
-    context.stroke();
+    context.moveTo(point.x, point.y - POINT_RADIUS);
+    context.lineTo(point.x, point.y + POINT_RADIUS);
+    context.stroke()
   }
 }
+
